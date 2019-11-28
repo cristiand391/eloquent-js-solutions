8079ce4227355200c6c5a90ffe41b08e4f9cb94b
Cristian Dominguez <cristiand391@protonmail.com>
2019-11-27 23:45:55 -0300

    Add "Quoting style" and "Numbers again"

---
+++ chapter9/numbers_again.js
@@ -0,0 +1,18 @@
+// Fill in this regular expression.
+
+// Not proud of this shit, but it works. I will check it again when I learn more about RE.
+let number = /^(\+|-)?(\d+\.?\d*|\.\d+)(e+(\+|-)?)?\d*$/i;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
---
+++ chapter9/quoting_style.js
@@ -0,0 +1,4 @@
+let text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/\B'/g, '"'));
+// → "I'm the cook," he said, "it's my job."
